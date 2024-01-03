5ae6c558cd2e863c58d4eff0815825dc03f740c0
bigth21 <taekhyeon.nam@gmail.com>
2024-01-03 19:39:21 +0900

    Create hello controller and API

---
+++ src/test/java/com/example/server/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(get("/api/v1/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello!"));
+    }
+}
