bc6c0e6de583b40a02fc4b2c9b4d0611759e7e71
bigth21 <taekhyeon.nam@gmail.com>
2024-01-06 16:11:57 +0900

    Create basic auth test code

--- src/test/java/com/example/server/HelloControllerTest.java
+++ src/test/java/com/example/server/HelloControllerTest.java
@@ -3,9 +3,11 @@ package com.example.server;
 import com.example.server.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,11 +17,20 @@ class HelloControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    SecurityProperties securityProperties;
 
     @Test
     void hello() throws Exception {
-        mockMvc.perform(get("/api/v1/hello"))
+        mockMvc.perform(get("/api/v1/hello")
+                        .with(httpBasic("user", securityProperties.getUser().getPassword())))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello!"));
+    }
+
+    @Test
+    void hello_401() throws Exception {
+        mockMvc.perform(get("/api/v1/hello"))
+                .andExpect(status().isUnauthorized());
     }
 }
