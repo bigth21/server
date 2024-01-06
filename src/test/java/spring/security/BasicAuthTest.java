59ede8b29a89ea20fb612fdfe31e913c777f5737
bigth21 <taekhyeon.nam@gmail.com>
2024-01-06 17:03:37 +0900

    Seperate security-related test code from project test code

--- src/test/java/spring/security/BasicAuthTest.java
+++ src/test/java/spring/security/BasicAuthTest.java
@@ -1,11 +1,14 @@
-package com.example.server;
+package spring.security;
 
-import com.example.server.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,8 +16,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = HelloController.class)
-class HelloControllerTest {
+@WebMvcTest
+public class BasicAuthTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -22,18 +25,34 @@ class HelloControllerTest {
     SecurityProperties securityProperties;
 
     @Test
-    void hello() throws Exception {
-        mockMvc.perform(get("/api/v1/hello")
+    void test() throws Exception {
+        mockMvc.perform(get("/test")
                         .with(httpBasic("user", securityProperties.getUser().getPassword())))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello!"))
+                .andExpect(content().string("test"))
                 .andDo(print());
     }
 
     @Test
-    void hello_401() throws Exception {
-        mockMvc.perform(get("/api/v1/hello"))
+    void test_401() throws Exception {
+        mockMvc.perform(get("/test"))
                 .andExpect(status().isUnauthorized())
                 .andDo(print());
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        TestController testController() {
+            return new TestController();
+        }
+    }
+
+    @RestController
+    static class TestController {
+        @GetMapping("/test")
+        public String test() {
+            return "test";
+        }
     }
 }
--- src/test/java/spring/security/InMemoryUserDetailsServiceTest.java
+++ src/test/java/spring/security/InMemoryUserDetailsServiceTest.java
@@ -1,27 +1,26 @@
-package com.example.server.web;
+package spring.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = SecurityConfigTest.TestController.class,
-includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
-class SecurityConfigTest {
+@WebMvcTest(controllers = InMemoryUserDetailsServiceTest.TestController.class)
+class InMemoryUserDetailsServiceTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -36,6 +35,21 @@ class SecurityConfigTest {
 
     @TestConfiguration
     static class TestConfig {
+        @Bean
+        UserDetailsService userDetailsService() {
+            var userDetailsService = new InMemoryUserDetailsManager();
+            var user = User.withUsername("user")
+                    .password("12345")
+                    .authorities("read")
+                    .build();
+            userDetailsService.createUser(user);
+            return userDetailsService;
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return NoOpPasswordEncoder.getInstance();
+        }
         @Bean
         TestController testController() {
             return new TestController();
